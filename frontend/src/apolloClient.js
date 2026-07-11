bf3e43cf4715df537ed2eac290b8a6bdde42cb46
Santu-Hansariafood <Santu@hansariafood.com>
2026-07-11 12:11:45 +0530

    changes one due

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: `${import.meta.env.VITE_API_URL || "http://localhost:25000"}/graphql`,
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:25001";
+const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:25000";
 
 class SocketService {
   constructor() {
