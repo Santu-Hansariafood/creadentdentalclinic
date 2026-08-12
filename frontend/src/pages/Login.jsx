e8dd2b0037009af2a9cc07322b4a2d4c55bb1f94
Santu-Hansariafood <Santu@hansariafood.com>
2026-08-12 10:25:41 +0530

    changes added with suspense

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,Suspense } from "react";
 import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { Phone, Lock, Eye, EyeOff, KeyRound, ArrowLeft } from "lucide-react";
